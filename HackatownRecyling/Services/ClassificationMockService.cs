a73c8324fa956663f5afa44556f331ff78e4b954
larrynet <lawrenceng3@hotmail.com>
2020-01-18 17:34:05 -0500

    Add MILwrapper and class mockup service.

---
+++ HackatownRecyling/Services/ClassificationMockService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HackatownRecyling.Interfaces;
+using Matrox.MatroxImagingLibrary;
+namespace HackatownRecyling.Services
+{
+    public class ClassificationMockService : IClassificationService
+    {
+        public void Classify(string filename)
+        {
+            
+        }
+    }
+}
