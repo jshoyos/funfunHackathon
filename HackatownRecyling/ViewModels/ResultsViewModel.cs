e6cc58ca13e1543694c645bd829288e9640b1509
JLens0 <julien.lenseigne.1@ens.etsmtl.ca>
2020-01-18 13:51:02 -0500

    Added results view

--- HackatownRecyling/ViewModels/ResultsViewModel.cs
+++ HackatownRecyling/ViewModels/ResultsViewModel.cs
@@ -1,12 +1,35 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Prism.Commands;
+using System;
+using System.Collections.ObjectModel;
 
 namespace HackatownRecyling.ViewModels
 {
     public class ResultsViewModel
     {
+        public ResultsViewModel()
+        {
+            RegisterCommands();
+        }
+
+        public DelegateCommand<int> FirstChoiceCommand { get; private set; }
+        public DelegateCommand<int> SecondChoiceCommand { get; private set; }
+        public DelegateCommand<int> ThirdChoiceCommand { get; private set; }
+
+        private void RegisterCommands()
+        {
+            FirstChoiceCommand = new DelegateCommand<int>(ChoiceCommandExecute, ChoiceCommandCanExecute);
+            SecondChoiceCommand = new DelegateCommand<int>(ChoiceCommandExecute, ChoiceCommandCanExecute);
+            ThirdChoiceCommand = new DelegateCommand<int>(ChoiceCommandExecute, ChoiceCommandCanExecute);
+        }
+
+        private bool ChoiceCommandCanExecute(int choiceIndex)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void ChoiceCommandExecute(int choiceIndex)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
