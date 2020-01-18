753837b3fbf99e5f575951927a9c765c9f634aea
JLens0 <julien.lenseigne.1@ens.etsmtl.ca>
2020-01-18 14:05:51 -0500

    added basic layout of result

--- HackatownRecyling/ViewModels/ResultsViewModel.cs
+++ HackatownRecyling/ViewModels/ResultsViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using Prism.Commands;
 using System;
 using System.Collections.ObjectModel;
+using System.Windows.Input;
 
 namespace HackatownRecyling.ViewModels
 {
@@ -11,25 +12,25 @@ namespace HackatownRecyling.ViewModels
             RegisterCommands();
         }
 
-        public DelegateCommand<int> FirstChoiceCommand { get; private set; }
-        public DelegateCommand<int> SecondChoiceCommand { get; private set; }
-        public DelegateCommand<int> ThirdChoiceCommand { get; private set; }
+        public ICommand FirstChoiceCommand { get; private set; }
+        public ICommand SecondChoiceCommand { get; private set; }
+        public ICommand ThirdChoiceCommand { get; private set; }
 
         private void RegisterCommands()
         {
-            FirstChoiceCommand = new DelegateCommand<int>(ChoiceCommandExecute, ChoiceCommandCanExecute);
-            SecondChoiceCommand = new DelegateCommand<int>(ChoiceCommandExecute, ChoiceCommandCanExecute);
-            ThirdChoiceCommand = new DelegateCommand<int>(ChoiceCommandExecute, ChoiceCommandCanExecute);
+            FirstChoiceCommand = new DelegateCommand<int?>(ChoiceCommandExecute, ChoiceCommandCanExecute);
+            SecondChoiceCommand = new DelegateCommand<int?>(ChoiceCommandExecute, ChoiceCommandCanExecute);
+            ThirdChoiceCommand = new DelegateCommand<int?>(ChoiceCommandExecute, ChoiceCommandCanExecute);
         }
 
-        private bool ChoiceCommandCanExecute(int choiceIndex)
+        private bool ChoiceCommandCanExecute(int? choiceIndex)
         {
-            throw new NotImplementedException();
+            return true;
         }
 
-        private void ChoiceCommandExecute(int choiceIndex)
+        private void ChoiceCommandExecute(int? choiceIndex)
         {
-            throw new NotImplementedException();
+            
         }
     }
 }
