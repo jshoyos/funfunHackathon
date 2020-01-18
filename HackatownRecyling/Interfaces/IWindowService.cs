d2c5f77d195a0487f6abae2ef1da5d890b5f5c61
JLens0 <julien.lenseigne.1@ens.etsmtl.ca>
2020-01-18 12:37:54 -0500

    Added comment to window service

--- HackatownRecyling/Interfaces/IWindowService.cs
+++ HackatownRecyling/Interfaces/IWindowService.cs
@@ -4,6 +4,10 @@
     {
         void ShowResultView();
         void ShowLiveCameraView();
+        /// <summary>
+        /// Starts the file dialog. Returns the file name or null if the user cancelled.
+        /// </summary>
+        /// <returns></returns>
         string ShowFileDialog();
     }
 }
