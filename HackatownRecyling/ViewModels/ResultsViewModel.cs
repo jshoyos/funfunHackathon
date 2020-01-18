c0e6a28ad7ce1fb3cff9484e1e06010acd2dfb28
Satiriques <bad_ghost2005@hotmail.com>
2020-01-18 15:43:40 -0500

    added event aggregator to resultsviewmodel

--- HackatownRecyling/ViewModels/ResultsViewModel.cs
+++ HackatownRecyling/ViewModels/ResultsViewModel.cs
@@ -1,19 +1,29 @@
 ﻿using HackatownRecyling.Common;
 using HackatownRecyling.Models;
 using Prism.Commands;
+using Prism.Events;
+using System;
 using System.Windows.Input;
+using System.Windows.Media.Imaging;
 
 namespace HackatownRecyling.ViewModels
 {
-    public class ResultsViewModel : BindableBase
+    public class ResultsViewModel : BindableBase, IDisposable
     {
-        public ResultsViewModel()
+        private BitmapImage _loadedImage;
+        private readonly IEventAggregator _eventAggregator;
+
+        public ResultsViewModel(IEventAggregator eventAggregator)
         {
             RegisterCommands();
+            
+            //eventAggregator.GetEvent<ImageLoadFromUserEvent>
+
             //todo: remove this, this is only for testing
-            FirstChoice = new ResultChoiceModel() { Name = "Cardboard", Score=0.9 };
+            FirstChoice = new ResultChoiceModel() { Name = "Cardboard", Score = 0.9 };
             SecondChoice = new ResultChoiceModel() { Name = "Glass", Score = 0.8 };
             ThirdChoice = new ResultChoiceModel() { Name = "Trash", Score = 0.6 };
+            _eventAggregator = eventAggregator;
         }
 
         public ICommand FirstChoiceCommand { get; private set; }
@@ -22,6 +32,19 @@ namespace HackatownRecyling.ViewModels
         public ResultChoiceModel FirstChoice { get; private set; }
         public ResultChoiceModel SecondChoice { get; private set; }
         public ResultChoiceModel ThirdChoice { get; private set; }
+        public BitmapImage LoadedImage
+        {
+            get => _loadedImage;
+            private set
+            {
+                if(_loadedImage != value)
+                {
+                    _loadedImage = value;
+                    OnPropertyChanged();
+                }
+               
+            }
+        }
 
         private void RegisterCommands()
         {
@@ -38,6 +61,11 @@ namespace HackatownRecyling.ViewModels
         private void ChoiceCommandExecute(int? choiceIndex)
         {
 
+        }
+
+        public void Dispose()
+        {
+            
         }
     }
 }
