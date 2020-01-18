624f96d64bce09d86431569193709468568fd76f
JLens0 <julien.lenseigne.1@ens.etsmtl.ca>
2020-01-18 12:16:28 -0500

    working on WindowService

---
+++ HackatownRecyling/Services/WindowService.cs
@@ -0,0 +1,19 @@
+﻿using HackatownRecyling.Interfaces;
+using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackatownRecyling.Services
+{
+    internal class WindowService : IWindowService
+    {
+        public void ShowFileDialog()
+        {
+            var dialog = new OpenFileDialog();
+            dialog.ShowDialog();
+        }
+    }
+}
